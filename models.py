9346ed1aec094df0cda2c746d6c7300e9893afec
Elijahrotimi <elijahrotimi@gmail.com>
2022-10-23 21:14:15 +0100

    Added manage.py and requirements.txt

--- models.py
+++ models.py
@@ -1,9 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 #from app import app
 from flask_login import UserMixin
+database_path = 'postgresql://postgres:postgres@localhost:5432/Idealabdb'
 
 db = SQLAlchemy()
-migrage = Migrate()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
 #Models
 
